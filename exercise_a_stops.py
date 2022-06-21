ed8736983cdd4cbe1fbaf2323ffaf4492001553a
Ethan <ethanbaird94@gmail.com>
2022-06-21 16:27:38 +0100

    finished a4

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -14,7 +14,14 @@ stops.insert(0, "Glasgow Queen Street")
 stops.insert(4, "Polmont")
 
 #4. Print out the index position of "Linlithgow"
+
+print(stops)
+print(stops.index("Linlithgow"))
+
 #5. Remove "Livingston" from the list using its name
+
+
+
 #6. Delete "Cumbernauld" from the list by index
 #7. Print the number of stops there are in the list
 #8. Sort the list alphabetically
